ea07314c5d2f008375904f2c6be8462eec3d770c
Yaindrop <yuan_dian@live.com>
2025-03-15 12:04:46 +0800

    wip

--- src/index.ts
+++ src/index.ts
@@ -3,64 +3,12 @@ import * as Y from 'yjs'
 
 import { ExtendsKeyof, OptionalKeyof, Overwrite } from './utils'
 
+// MARK: YType
+// Only support YText, YArray, YMap for now
 export type YType = YText | YArray<any> | YMap<any>
 
-type YTypeRecord = Record<string, YType>
-
 // YArray / YMap values can be either YType or Json
-type Value = YType | Json
-
-namespace Value {
-  export function fromSeed<S extends ValueSeed>(seed: S): ValueOf<S> {
-    if (typeof seed === 'object' && seed !== null) {
-      if (YTextSeedSymbol in seed) {
-        return YText.fromSeed(seed) as ValueOf<S>
-      } else if (YArraySeedSymbol in seed) {
-        return YArray.fromSeed(seed) as ValueOf<S>
-      } else if (YMapSeedSymbol in seed) {
-        return YMap.fromSeed(seed) as ValueOf<S>
-      }
-    }
-    return seed as ValueOf<S>
-  }
-}
-
-type ValueRecord = Record<string, Value>
-
-// MARK: seed
-type YTypeSeed = YTextSeed | YArraySeed<any> | YMapSeed<any>
-
-type ValueSeed = YTypeSeed | Json
-
-type ValueRecordSeed = Record<string, ValueSeed>
-
-// value to seed
-type SeedOf<V extends ValueRecord | Value> = V extends YText
-  ? YTextSeed
-  : V extends YArray<infer TValue>
-    ? YArraySeed<TValue>
-    : V extends YMap<infer R>
-      ? YMapSeed<R>
-      : V extends Json
-        ? V
-        : never
-
-type RecordSeedOf<V extends ValueRecord> = { [K in keyof V]: SeedOf<V[K]> }
-
-// seed to value
-type ValueOf<S extends ValueRecordSeed | ValueSeed> = S extends YTextSeed
-  ? YText
-  : S extends YArraySeed<infer TValue>
-    ? YArray<TValue>
-    : S extends YMapSeed<infer R>
-      ? YMap<R>
-      : S extends Json
-        ? S
-        : never
-
-type ValueRecordOf<S extends ValueRecordSeed> = {
-  [K in keyof S]: ValueOf<S[K]>
-}
+export type YValue = YType | Json
 
 // MARK: json
 type Json = JsonPrimitive | JsonArray | JsonObject
@@ -68,26 +16,90 @@ type JsonPrimitive = string | number | boolean | null
 type JsonArray = Json[]
 type JsonObject = Partial<{ [key: string]: Json }>
 
-type ToJson<V extends Value> = V extends Json
-  ? V
-  : V extends YText
-    ? string
+export type JsonOf<V extends YValue> =
+  V extends YMap<infer R>
+    ? { [K in keyof R]: JsonOf<R[K]> }
     : V extends YArray<infer TValue>
-      ? ToJson<TValue>[]
-      : V extends YMap<infer R>
-        ? {
-            [K in keyof R]: ToJson<R[K]>
-          }
-        : never
+      ? JsonOf<TValue>[]
+      : V extends YText
+        ? string
+        : V extends Json
+          ? V
+          : never
+
+export type JsonOfYDoc<R extends YDocRecord> = {
+  [K in keyof R]: JsonOf<R[K]>
+}
+
+// MARK: YText
+export type YText = Y.Text
+
+export namespace YText {
+  export function from(initialValue: string): YText {
+    return new Y.Text(initialValue)
+  }
+}
+
+// MARK: YArray
+type YArrayType<V extends YValue> = {
+  get(index: number): V | undefined
+  toJSON(): JsonOf<V>[]
+}
+
+export type YArray<V extends YValue> = Overwrite<Y.Array<V>, YArrayType<V>>
+
+export namespace YArray {
+  export function from<V extends YValue>(initialValue: V[]): YArray<V> {
+    return Y.Array.from(initialValue as any[]) as unknown as YArray<V>
+  }
+}
+
+// MARK: YMap
+type YMapRecord = Record<string, YValue>
+
+type YMapEntry<R extends YMapRecord> = {
+  [key in keyof R]-?: [key, Required<R>[key]]
+}[keyof R]
+
+type YMapType<R extends YMapRecord> = {
+  clone(): YMap<R>
+  keys(): IterableIterator<keyof R>
+  values(): IterableIterator<R[keyof R]>
+  entries(): IterableIterator<YMapEntry<R>>
+  forEach(fn: (key: keyof R, value: R[keyof R], self: YMap<R>) => void): void
+  delete(key: OptionalKeyof<R>): void
+  set<K extends keyof R, V extends R[K]>(key: K, value: V): V
+  get<K extends keyof R>(key: K): R[K]
+  has(key: keyof R): boolean
+  toJSON(): {
+    [K in keyof R]: JsonOf<R[K]>
+  }
+  [Symbol.iterator](): IterableIterator<YMapEntry<R>>
+}
+
+export type YMap<R extends YMapRecord> = Overwrite<Y.Map<R>, YMapType<R>>
+
+export namespace YMap {
+  export function from<R extends YMapRecord>(initialValue: R): YMap<R> {
+    const yMap = new Y.Map() as unknown as YMap<R>
+    for (const k in initialValue) {
+      const v = initialValue[k]
+      yMap.set(k, v)
+    }
+    return yMap
+  }
+}
 
 // MARK: YDoc
+type YDocRecord = Record<string, YType>
+
 type YTypeConstructor<T extends YType> = new () => T extends YMap<any>
   ? YMap<any>
   : T extends YArray<any>
     ? YArray<any>
     : YText
 
-type YDocType<R extends YTypeRecord> = {
+type YDocType<R extends YDocRecord> = {
   get<K extends keyof R, TValue extends R[K]>(name: K, TypeConstructor: YTypeConstructor<TValue>): R[K]
   get<K extends keyof R>(name: K): R[K] | undefined
 
@@ -96,143 +108,39 @@ type YDocType<R extends YTypeRecord> = {
   getMap<K extends ExtendsKeyof<R, YMap<any>>>(name: K): R[K]
 }
 
-export type YDoc<R extends YTypeRecord> = Overwrite<Y.Doc, YDocType<R>>
+// YDoc does not allow adding constructed YTypes, so we need to use DocSchema to define the top level structure of the YDoc
+type DocSchema<R extends YDocRecord> = {
+  [K in keyof R]: R[K] extends YText
+    ? string
+    : R[K] extends YArray<infer V>
+      ? V[]
+      : R[K] extends YMap<infer Record>
+        ? Record
+        : never
+}
+
+export type YDoc<R extends YDocRecord> = Overwrite<Y.Doc, YDocType<R>>
 
 export namespace YDoc {
-  export function from<R extends YTypeRecord>(obj: {
-    [K in keyof R]: SeedOf<R[K]>
-  }): YDoc<R> {
+  export function from<R extends YDocRecord>(schema: DocSchema<R>): YDoc<R> {
     const yDoc = new Y.Doc()
-    yDoc.transact(() => {
-      for (const [key, value] of Object.entries(obj)) {
-        if (YTextSeedSymbol in value) {
-          const yText = yDoc.getText(key)
-          YText.applySeed(yText, value as YTextSeed)
-        } else if (YArraySeedSymbol in value) {
-          const yArray = yDoc.getArray(key)
-          YArray.applySeed(yArray, value as YArraySeed<any>)
-        } else if (YMapSeedSymbol in value) {
-          const yMap = yDoc.getMap(key)
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-          YMap.applySeed(yMap as any, value as YMapSeed<any>)
+    for (const key in schema) {
+      const value = schema[key]
+      if (typeof value === 'string') {
+        const yText = yDoc.getText(key)
+        yText.insert(0, value)
+      } else if (Array.isArray(value)) {
+        const yArray = yDoc.getArray(key)
+        yArray.insert(0, value)
+      } else if (typeof value === 'object') {
+        const yMap = yDoc.getMap(key)
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+        for (const k in value as YMapRecord) {
+          const v = value[k]
+          yMap.set(k, v)
         }
       }
-    })
-    return yDoc as unknown as YDoc<R>
-  }
-}
-
-// MARK: YText
-const YTextSeedSymbol = Symbol('YTextSeed')
-type YTextSeed = {
-  [YTextSeedSymbol]: unknown
-  value: string
-}
-
-export type YText = Y.Text
-
-export namespace YText {
-  export function seed(str: string): YTextSeed {
-    return Object.defineProperty({ value: str }, YTextSeedSymbol, {
-      value: YTextSeedSymbol,
-      writable: false,
-      enumerable: false,
-    }) as YTextSeed
-  }
-
-  export function fromSeed(seed: YTextSeed): YText {
-    return new Y.Text(seed.value)
-  }
-
-  export function applySeed(yText: YText, seed: YTextSeed): void {
-    yText.insert(0, seed.value)
-  }
-}
-
-// MARK: YArray
-const YArraySeedSymbol = Symbol('YArraySeed')
-type YArraySeed<V extends Value> = {
-  [YArraySeedSymbol]: unknown
-  value: SeedOf<V>[]
-}
-
-type YArrayType<V extends Value> = {
-  get(index: number): V | undefined
-  toJSON(): ToJson<V>[]
-}
-
-export type YArray<V extends Value> = Overwrite<Y.Array<V>, YArrayType<V>>
-
-export namespace YArray {
-  export function seed<S extends ValueSeed>(arr: S[]): YArraySeed<ValueOf<S>> {
-    return Object.defineProperty({ value: arr }, YArraySeedSymbol, {
-      value: YArraySeedSymbol,
-      writable: false,
-      enumerable: false,
-    }) as YArraySeed<ValueOf<S>>
-  }
-
-  export function fromSeed<S extends ValueSeed>(seed: YArraySeed<ValueOf<S>>): YArray<ValueOf<S>> {
-    const yArray = new Y.Array() as unknown as YArray<ValueOf<S>>
-    applySeed(yArray, seed)
-    return yArray
-  }
-
-  export function applySeed<S extends ValueSeed>(yArray: YArray<ValueOf<S>>, seed: YArraySeed<ValueOf<S>>): void {
-    yArray.insert(0, seed.value.map(Value.fromSeed) as ValueOf<S>[])
-  }
-}
-
-// MARK: YMap
-const YMapSeedSymbol = Symbol('YMapSeed')
-type YMapSeed<R extends ValueRecord> = {
-  [YMapSeedSymbol]: typeof YMapSeedSymbol
-  value: RecordSeedOf<R>
-}
-
-type EntryType<R extends ValueRecord> = {
-  [key in keyof R]-?: [key, Required<R>[key]]
-}[keyof R]
-
-type YMapType<R extends ValueRecord> = {
-  clone(): YMap<R>
-  keys(): IterableIterator<keyof R>
-  values(): IterableIterator<R[keyof R]>
-  entries(): IterableIterator<EntryType<R>>
-  forEach(fn: (key: keyof R, value: R[keyof R], self: YMap<R>) => void): void
-  delete(key: OptionalKeyof<R>): void
-  set<K extends keyof R, V extends R[K]>(key: K, value: V): V
-  get<K extends keyof R>(key: K): R[K]
-  has(key: keyof R): boolean
-  toJSON(): {
-    [K in keyof R]: ToJson<R[K]>
-  }
-  [Symbol.iterator](): IterableIterator<EntryType<R>>
-}
-
-export type YMap<R extends ValueRecord> = Overwrite<Y.Map<R>, YMapType<R>>
-
-export namespace YMap {
-  export function seed<S extends ValueRecordSeed>(obj: S): YMapSeed<ValueRecordOf<S>> {
-    return Object.defineProperty({ value: obj }, YMapSeedSymbol, {
-      value: YMapSeedSymbol,
-      writable: false,
-      enumerable: false,
-    }) as YMapSeed<ValueRecordOf<S>>
-  }
-
-  export function fromSeed<S extends ValueRecordSeed>(seed: YMapSeed<ValueRecordOf<S>>): YMap<ValueRecordOf<S>> {
-    const yMap = new Y.Map() as unknown as YMap<ValueRecordOf<S>>
-    applySeed(yMap, seed)
-    return yMap
-  }
-
-  export function applySeed<S extends ValueRecordSeed>(
-    yMap: YMap<ValueRecordOf<S>>,
-    seed: YMapSeed<ValueRecordOf<S>>,
-  ): void {
-    for (const [k, v] of Object.entries(seed.value)) {
-      yMap.set(k, Value.fromSeed(v) as ValueRecordOf<S>[string])
     }
+    return yDoc as unknown as YDoc<R>
   }
 }
